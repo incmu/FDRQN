9b6e8f286ffacf6433cbf7fc6723afb92b6745e9
Kenan Piljević <62318470+incmu@users.noreply.github.com>
2023-09-28 23:58:11 +0200

    rework

--- main.py
+++ main.py
@@ -1,71 +1,45 @@
-# Import your models, agents, preprocessors, environments, and other necessary components
-from environments.db_environment import DatabaseEnvironment
-from agents.miniAgents.nlp_agent import NLPAgent
-from agents.miniAgents.rnn_agent import RNNModelAgent
-from agents.miniAgents.fnn_agent import FNNAgent
-from agents.combinedAgents.nlnn_agent import NLNNAgent
-from agents.superAgent import SuperAgent
-from preprocessing.nlp_data import load_and_preprocess_data as load_and_preprocess_nlp_data
-from preprocessing.rnn_data import load_and_preprocess_data as load_and_preprocess_rnn_data
-from preprocessing.fnn_data import load_and_preprocess_data as load_and_preprocess_fnn_data
+import numpy as np
 
-# File Path
-file_path = "datasets/js_dataset/javas.csv"
+from preprocessing.nlp_data import load_and_preprocess_data  # Assuming this is where your preprocessed data and config are stored
+from models.nlp.nlp_model import NLPModel  # Import your NLP model class
+from models.seq2seq.seq2seq_model import Seq2SeqModel  # Import your Seq2Seq model class
+from models.rnn.rnn_model import RNNModel  # Import your RNN model class
 
 # Load and preprocess data
-nlp_data = load_and_preprocess_nlp_data(file_path)
-rnn_data = load_and_preprocess_rnn_data(file_path, 100)
-fnn_data = load_and_preprocess_fnn_data(file_path)
+file_path = 'datasets/js_dataset/javas.csv'  # Specify the actual file path for your data
+data = load_and_preprocess_data(file_path)
+X = data['X']
+Y = data['Y']
+config = data['config']  # Added this line to get the config
 
-# Initialize Models Configurations
-nlp_model_config = {
-    "vocab_size": 10000,
-    "embedding_dim": 64,
-    "input_length": 120,
-    "num_classes": 3
-}
+nlp_model = NLPModel(data['config'], input_length=data['X'].shape[1], num_classes=data['Y'].shape[1])
+  # Use config to initialize NLPModel
 
-rnn_model_config = {
-    "input_shape": rnn_data['config']['input_shape'],
-    "num_classes": rnn_data['config']['num_classes']
-}
+print("Shape of X:", data['X'].shape)
+print("Shape of Y:", data['Y'].shape)
 
-fnn_model_config = {
-    "input_dim": fnn_data['config']['num_features'],
-    "num_classes": fnn_data['config']['num_classes']
-}
 
-# Initialize Database Environment
-db_environment = DatabaseEnvironment()
+nlp_model.train(data['X'], data['Y'], epochs=10, batch_size=32)  # Adjust epochs and batch_size as needed
+  # Adjust epochs and batch_size as needed
 
-# Initialize Mini Agents
-nlp_agent = NLPAgent(nlp_model_config, environment=db_environment, data_file_path=file_path)
-rnn_agent = RNNModelAgent(rnn_model_config, environment=db_environment, data_file_path=file_path)
-fnn_agent = FNNAgent(fnn_model_config, environment=db_environment, data_file_path=file_path)
+# Get NLP Model output to feed into Seq2Seq Model
+nlp_output = nlp_model.predict(X)
+nlp_output_sequence_length = nlp_output.shape[1]
+Y_sequence_length = Y.shape[1]
+embedding_dim = 256
 
-# Initialize Combined Agents
-nlnn_agent = NLNNAgent(nlp_model_config=nlp_model_config, rnn_model_config=rnn_model_config,
-                       environment=db_environment, data_file_path=file_path)
 
-big_agent_config = {
-    "nlnn_agent": nlnn_agent,
-    "fnn_agent": fnn_agent
-}
+# Initialize and Train Seq2Seq Model (Assuming you have an appropriate model and data preparation)
+seq2seq_model = Seq2SeqModel(input_shape=(nlp_output_sequence_length, embedding_dim), num_classes=config['num_classes'])
+seq2seq_model.train(nlp_output, Y, epochs=10, batch_size=32)  # Adjust epochs and batch_size as needed
 
-state_size = 10  # Define appropriately
-action_size = 5  # Define appropriately
+# Get Seq2Seq Model output to feed into RNN Model
+seq2seq_output = seq2seq_model.predict(nlp_output)
 
-ppo_agent_config = {
-    "state_size": state_size,
-    "action_size": action_size,
-    "environment": db_environment,
-    "data_file_path": file_path
-}
+# Initialize and Train RNN Model
+rnn_model = RNNModel(input_shape=seq2seq_output.shape, num_classes=config['num_classes'])
+rnn_model.train(seq2seq_output, Y, epochs=10, batch_size=32)  # Adjust epochs and batch_size as needed
 
-# Initialize Super Agent
-super_agent = SuperAgent(big_agent_config, ppo_agent_config, db_environment)
+print("Training Complete!")
 
-# Run the Super Agent
-total_reward = super_agent.run_episode()
-
-print("Total Reward for Episode:", total_reward)
+# You can now use nlp_model, seq2seq_model, and rnn_model for predictions.
